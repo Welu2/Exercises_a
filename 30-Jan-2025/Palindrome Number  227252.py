2437726cfd893552083b908f772fe4f60f749fdd
Welu2 <123329083+Welu2@users.noreply.github.com>
2025-01-30 18:25:55 +0300

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 30-Jan-2025/Palindrome Number  227252.py
@@ -0,0 +1,7 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
