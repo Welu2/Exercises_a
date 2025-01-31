6e4162b2f818cbb7b35f9739e3b4443cd515e771
Welu2 <123329083+Welu2@users.noreply.github.com>
2025-01-31 16:14:14 +0300

    Created 'Rotate String' by A2SV Hub

---
+++ 31-Jan-2025/Rotate String 230988.py
@@ -0,0 +1,12 @@
+# Problem: Rotate String - https://leetcode.com/problems/rotate-string/
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for i in range(len(s)):
+            if s[i] != goal[0]:
+                continue
+            else:
+                if s[i:]+s[:i] == goal:
+                    return True
+            
+        return False
