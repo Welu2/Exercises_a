400645b2f72585371eb1dd97a3501dc41fde012a
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-03 19:46:58 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 03-Mar-2025/Jump Game 289887.py
@@ -0,0 +1,12 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        goal = len(nums) -1
+        l=goal - 1
+        while l >= 0:   
+            if (goal-l) <= nums[l]:
+                goal=l
+            l-=1
+
+        return goal == 0
