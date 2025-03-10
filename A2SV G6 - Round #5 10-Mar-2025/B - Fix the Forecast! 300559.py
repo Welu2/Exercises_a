e11afb02baba83ac5c62adc28507046be50b414f
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-10 19:48:52 +0300

    Created 'B - Fix the Forecast!' by A2SV Hub

---
+++ A2SV G6 - Round #5 10-Mar-2025/B - Fix the Forecast! 300559.py
@@ -0,0 +1,23 @@
+# Problem: B - Fix the Forecast! - https://codeforces.com/gym/591928/problem/B
+
+t=int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    arr1=list( map(int,input().split()))
+    arr2=list( map(int,input().split()))
+    arr1 = [(arr1[i], i) for i in range(n)]
+    arr1.sort()
+    arr2.sort()
+  
+    ans=[0]*n
+
+    for i in range(n):
+        _, ind = arr1[i]
+        ans[ind] = arr2[i]
+  
+
+
+    print(*ans)
+
+
+
