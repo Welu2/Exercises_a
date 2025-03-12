adf9b34c16053b1a931ec25e5d00dfab6498fc0e
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-12 14:23:59 +0300

    Created 'Kuriyama Mirai's Stones' by A2SV Hub

---
+++ 12-Mar-2025/Kuriyama Mirai's Stones 303770.py
@@ -0,0 +1,29 @@
+# Problem: Kuriyama Mirai's Stones - https://codeforces.com/contest/433/problem/B
+
+n=int(input())
+arr=list(map(int,input().split()))
+questions=int(input())
+values=arr.copy()
+
+def prefixsum(arr):
+    for i in range(1,len(arr)):
+        arr[i]+=arr[i-1]
+    return arr
+unSorted=prefixsum(values)
+
+arr.sort()
+arranged=prefixsum(arr)
+
+for _ in range(questions):
+    type,start,end=map(int,input().split())
+    if type == 1:
+        if start==1:
+            print(unSorted[end-1])
+        else:
+           print(unSorted[end-1]-unSorted[start-2]) 
+    else:
+        if start==1:
+            print(arranged[end-1])
+        else:
+            print(arranged[end-1]-arranged[start-2])
+
