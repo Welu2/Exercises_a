1c45a6b4fd517d5d1ecef8631d5cfbcdcff364e4
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-12 14:44:30 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 12-Mar-2025/Array Splitting 303790.py
@@ -0,0 +1,19 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+
+n,k = map(int,input().split())
+arr=list(map(int,input().split()))
+if n==k :
+    print(0)
+else:
+    gaps = []
+    for i in range(1, n):
+        gaps.append(arr[i] - arr[i - 1])
+    
+    gaps.sort(reverse=True)
+      
+        
+    total_cost = arr[-1] - arr[0]
+   
+    
+    print(total_cost - sum(gaps[:k-1]))
