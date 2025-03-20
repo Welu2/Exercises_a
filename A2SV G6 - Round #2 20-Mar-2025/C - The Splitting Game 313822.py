4ff39f5b4ebeb29f1527be527e0d9799699de4ed
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-20 23:37:31 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 20-Mar-2025/C - The Splitting Game 313822.py
@@ -0,0 +1,22 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+n=int(input())
+
+for _ in range(n):
+    t=int(input())
+    s=input()
+    whole=Counter(s)
+    leftOver=Counter()
+    maxDistinct=0
+    for char in s:
+        whole[char]-=1
+        leftOver[char]+=1
+        if whole[char] == 0:
+            del whole[char]
+        maxDistinct=max(maxDistinct,(len(whole)+len(leftOver)))
+
+    print(maxDistinct)
+
+    
+    
