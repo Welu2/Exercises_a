978110fb2211caa9f57bdc3a69987bcfbf05a8f7
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-20 23:23:03 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 20-Mar-2025/E - From S To T 313793.py
@@ -0,0 +1,29 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+from collections import Counter
+t= int(input())
+for _ in range(t):
+    s=input()
+    t=input()
+    p=input()
+    flag=True
+    i=0
+    j=0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i+=1
+        j+=1
+    if i != len(s):
+        flag=False
+    requried=Counter(t)
+    available=Counter(s+p)
+    for i in requried:
+        if requried[i] > available[i]:
+            flag=False
+            break
+
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+
