f0f71e7cb7033601c0ad730ca8347f3aa1219cbc
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-21 01:32:18 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 20-Mar-2025/F - Array Transformation 313919.py
@@ -0,0 +1,17 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    apos=Counter(arr)
+    possible=list(apos.values())
+    possible.sort()
+    minV=float("inf")
+    for i in range(len(possible)):
+        curmin=n-(len(possible)-i)*possible[i]
+
+        minV=min(minV,curmin)
+
+    print(minV)
