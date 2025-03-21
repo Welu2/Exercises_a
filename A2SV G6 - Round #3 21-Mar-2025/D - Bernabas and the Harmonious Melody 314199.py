387cc0e395fd9f02224bea7ea14b4bc31cc24bc5
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-21 14:40:51 +0300

    Created 'D - Bernabas and the Harmonious Melody' by A2SV Hub

---
+++ A2SV G6 - Round #3 21-Mar-2025/D - Bernabas and the Harmonious Melody 314199.py
@@ -0,0 +1,45 @@
+# Problem: D - Bernabas and the Harmonious Melody - https://codeforces.com/gym/588468/problem/D
+
+t=int(input())
+def check(c):
+    l=0
+    r=len(s)-1
+    deleted=0
+    while l < r:
+        if s[l] != s[r]:
+            if s[l] == c:
+                l+=1
+                deleted+=1
+            elif s[r] == c:
+                r-=1
+                deleted+=1
+            else:
+                return float('inf')
+
+        else:
+            l+=1
+            r-=1
+    return deleted
+for _ in range(t):
+    n=int(input())
+    s=input()
+    is_pali=True
+    l,r=0,n-1
+    while l < r:
+        if s[l] != s[r]:
+            is_pali=False
+            break
+        else:
+            l+=1
+            r-=1
+    if is_pali:
+        print(0)
+
+    else:
+        
+        ans=min(check(s[l]),check(s[r]))
+
+        if ans == float("inf"):
+            print(-1)
+        else:
+            print(ans)
