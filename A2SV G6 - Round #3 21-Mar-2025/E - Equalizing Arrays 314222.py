ed673844045d0eb419c2c691de24d4d3d50ec1ab
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-03-21 14:58:46 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 21-Mar-2025/E - Equalizing Arrays 314222.py
@@ -0,0 +1,30 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n1=int(input())
+arr1=list(map(int,input().split()))
+n2=int(input())
+arr2=list(map(int,input().split()))
+if sum(arr1) != sum(arr2):
+    print(-1)
+else:
+    cursum1,cursum2=0,0
+    l1,l2=0,0
+    ans=0
+    while l1 < n1:
+        cursum1+=arr1[l1]
+        cursum2+=arr2[l2]
+        l1+=1
+        l2+=1
+
+        while cursum1 != cursum2:
+            if cursum1 < cursum2:
+                cursum1+=arr1[l1]
+                l1+=1
+            else:
+                cursum2+=arr2[l2]
+                l2+=1
+        ans+=1
+       
+    
+    print(ans)
+
