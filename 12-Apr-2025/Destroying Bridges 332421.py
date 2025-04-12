0a322781ca1edec8c9794bb1bfe84945cbdccbcf
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-04-12 06:42:25 +0300

    Created 'Destroying Bridges' by A2SV Hub

---
+++ 12-Apr-2025/Destroying Bridges 332421.py
@@ -0,0 +1,12 @@
+# Problem: Destroying Bridges - https://codeforces.com/problemset/problem/1944/A
+
+t=int(input())
+for _ in range(t):
+    n,k = map(int,input().split())
+    if k == 0:
+        print(n)
+    else:
+        for i in range(1,n+1):
+            if i*(n-i)<=k:
+                print(i)
+                break
