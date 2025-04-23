340b119250dcdce0108e6778d305f27e0669047f
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-04-23 19:25:33 +0300

    Created 'C - Ras Alula and The Decrypted Messages' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 23-Apr-2025/C - Ras Alula and The Decrypted Messages 339638.py
@@ -0,0 +1,29 @@
+# Problem: C - Ras Alula and The Decrypted Messages - https://codeforces.com/gym/601269/problem/C
+
+t=int(input())
+for _ in range(t):
+    n,k=map(int,input().split())
+    d=input()
+    w=input()
+    flag=False
+    expected=0
+    current=0
+    for i in w:
+        expected+=ord(i)
+
+    for j in range(k):
+        current+=ord(d[j])
+
+    if current == expected:
+        flag=True
+    for j in range(k,n):
+        current+=ord(d[j])
+        current-=ord(d[j-k])
+        if current == expected:
+            flag=True
+    
+    if flag:
+        print("YES")
+    else:
+        print("NO")
+   
