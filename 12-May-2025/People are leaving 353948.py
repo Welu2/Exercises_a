e3278f57357f04a46b820d80fad1b79e88378f80
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-05-12 22:12:19 +0300

    Created 'People are leaving' by A2SV Hub

---
+++ 12-May-2025/People are leaving 353948.py
@@ -0,0 +1,38 @@
+# Problem: People are leaving - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/A
+
+import sys
+input = sys.stdin.read
+sys.setrecursionlimit(1 << 25)
+
+def solve():
+    data = input().split()
+    n = int(data[0])
+    m = int(data[1])
+    
+    next_alive = list(range(n + 2))  # index 1 to n; n+1 is dummy for bounds
+
+    def find(x):
+        if next_alive[x] != x:
+            next_alive[x] = find(next_alive[x])
+        return next_alive[x]
+
+    index = 2
+    result = []
+
+    for _ in range(m):
+        op = data[index]
+        x = int(data[index + 1])
+        index += 2
+
+        if op == '-':
+            next_alive[x] = find(x + 1)
+        else:  # op == '?'
+            res = find(x)
+            if res > n:
+                result.append("-1")
+            else:
+                result.append(str(res))
+
+    print("\n".join(result))
+
+solve()
