d125dec6e29ac07ff987b5a7c9da8627d97e7a69
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-06-18 18:48:17 +0300

    Created 'Find Center of Star Graph' by A2SV Hub

---
+++ 18-Jun-2025/Find Center of Star Graph 367834.py
@@ -0,0 +1,11 @@
+# Problem: Find Center of Star Graph - https://leetcode.com/problems/find-center-of-star-graph/
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        graph=defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        for ed in graph:
+            if len(graph[ed]) == len(graph)-1:
+                return ed
