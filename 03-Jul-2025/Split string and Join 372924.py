3811dee6afea006f39ccafea24c6bc87a98f3365
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-07-03 15:56:20 +0300

    Created 'Split string and Join' by A2SV Hub

---
+++ 03-Jul-2025/Split string and Join 372924.py
@@ -0,0 +1,15 @@
+# Problem: Split string and Join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+
+def split_and_join(line):
+    # write your code here
+    a=line.split(" ")
+    a="-".join(a)
+    return a
+    
+    
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
