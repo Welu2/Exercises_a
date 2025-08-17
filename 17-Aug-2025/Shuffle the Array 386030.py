f6f2e1ff6225ad2100c27722bb613e60b6d524ab
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-08-17 21:14:00 +0300

    Created 'Shuffle the Array' by A2SV Hub

---
+++ 17-Aug-2025/Shuffle the Array 386030.py
@@ -0,0 +1,10 @@
+# Problem: Shuffle the Array - https://leetcode.com/problems/shuffle-the-array/
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        a=[]
+        for i in range(n):
+            a.append(nums[i])
+            a.append(nums[i+n])
+
+        return a
