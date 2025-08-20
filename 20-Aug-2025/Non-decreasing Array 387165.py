a3230243be16cb45873b1ca8e2c074c96f110030
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-08-20 18:27:39 +0300

    Created 'Non-decreasing Array' by A2SV Hub

---
+++ 20-Aug-2025/Non-decreasing Array 387165.py
@@ -0,0 +1,17 @@
+# Problem: Non-decreasing Array - https://leetcode.com/problems/non-decreasing-array/
+
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        count = 0 
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                if count == 1:
+                    return False  
+                count += 1
+
+               
+                if i == 1 or nums[i] >= nums[i - 2]:
+                    nums[i - 1] = nums[i]  
+                else:
+                    nums[i] = nums[i - 1]  
+        return True
