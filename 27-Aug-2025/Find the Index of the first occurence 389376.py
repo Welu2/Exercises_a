10b2d915669904dfa9b2e8b484f5af16678e34e9
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-08-27 22:43:39 +0300

    Created 'Find the Index of the first occurence' by A2SV Hub

---
+++ 27-Aug-2025/Find the Index of the first occurence 389376.py
@@ -0,0 +1,7 @@
+# Problem: Find the Index of the first occurence - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        return -1
