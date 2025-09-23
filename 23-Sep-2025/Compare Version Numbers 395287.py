e4ea27c34601d295498155f0160497d5e7408f21
Welela Bekele <123329083+Welu2@users.noreply.github.com>
2025-09-23 19:56:30 +0300

    Created 'Compare Version Numbers' by A2SV Hub

---
+++ 23-Sep-2025/Compare Version Numbers 395287.py
@@ -0,0 +1,19 @@
+# Problem: Compare Version Numbers - https://leetcode.com/problems/compare-version-numbers/
+
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1_parts = list(map(int, version1.split('.')))
+        v2_parts = list(map(int, version2.split('.')))
+        
+        max_len = max(len(v1_parts), len(v2_parts))
+        
+        # Pad with zeros to make both lists equal in length
+        v1_parts.extend([0] * (max_len - len(v1_parts)))
+        v2_parts.extend([0] * (max_len - len(v2_parts)))
+        
+        for i in range(max_len):
+            if v1_parts[i] > v2_parts[i]:
+                return 1
+            elif v1_parts[i] < v2_parts[i]:
+                return -1
+        return 0
